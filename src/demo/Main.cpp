d2c2f92d7310f0154c9fe20cb32721a34c42e8f9
Charlotte Mackay <77455208+CharlotteM7@users.noreply.github.com>
2023-05-07 10:04:12 +0100

    Update Main.cpp

--- src/demo/Main.cpp
+++ src/demo/Main.cpp
@@ -1,9 +1,13 @@
 #include "MyGame.h"
+#include "../engine/custom/MyEngineSystem.h"
+
 
 int main(int argc, char * args[]) {
 
 	try {
-        MyGame game;
+		MyEngineSystem engineSystem;
+		engineSystem.perlin_init();
+		MyGame game;
 		game.runMainLoop();
 	} catch (EngineException & e) {
 		std::cout << e.what() << std::endl;
